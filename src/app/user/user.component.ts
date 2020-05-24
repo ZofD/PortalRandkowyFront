8034ab743d3f11575fae291171b2b479dc5b7944
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-05-24 13:56:47 +0200

    dodanie bootstrapa i JQuery. Stworzenie koncepcyjnego panelu logowania/strony glownej

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { USERS } from '../mock-users';
+import {User} from './user';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  users = USERS;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
