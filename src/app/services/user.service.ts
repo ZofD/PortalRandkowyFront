6818fca059e805b97e804e69d76cc473bf325541
banaskrzysztof <47889449+banaskrzysztof@users.noreply.github.com>
2020-06-18 14:06:01 +0200

    Wstępna edycja własnego profilu przez użytkownika

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Login} from '../log-in/log-in.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  public zalogowanyUzytkownik: boolean = JSON.parse(localStorage.getItem('zalogowany'));
+  public isLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject(this.zalogowanyUzytkownik !== null ? this.zalogowanyUzytkownik : false);
 
-  constructor() { }
+  host = 'http://localhost:8081/';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getAllUser() {
+    return this.http.get(this.host + 'api/uzytkownik');
+  }
+
+  public deleteUser(body) {
+    return this.http.request('delete', this.host + 'api/uzytkownik', {body});
+  }
+
+  public addUser(body): Observable<any> {
+    return this.http.post(this.host + 'api/uzytkownik', body);
+  }
+
+  public updateUser(body) {
+    return this.http.put(this.host + 'api/uzytkownik', body);
+  }
+
+  public existUser(data: Login) {
+    return this.http.get(this.host + 'api/uzytkownik/nick/' + data.haslo + '/password/' + data.mail);
+  }
 }
