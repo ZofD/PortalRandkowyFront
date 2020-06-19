44e961319673045b697f9d61c5424116476ebac2
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-06-19 14:20:25 +0200

    dodanie dodawania ;) zdjec

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -51,7 +51,7 @@ export class LogInComponent implements OnInit {
   }
 
   public addUser(newUser: New): void {
-    if (newUser.plec !== ('Kobieta' || 'Mężczyzna') || newUser.mail.length === 0) {
+    if (newUser.plec !== ('Kobieta' && 'Mężczyzna') || newUser.mail.length === 0) {
       this.errorRejestracja = true;
     } else {
       this.userService.setNewUser(newUser);
