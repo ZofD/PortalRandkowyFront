7c0e6513ac627ae5c61977968481f8e61f5c5ff2
banaskrzysztof <47889449+banaskrzysztof@users.noreply.github.com>
2020-06-20 20:15:34 +0200

    Wyszukiwanie z danych z bazy

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -2,7 +2,7 @@ import {ChangeDetectorRef, Component, OnInit, Input} from '@angular/core';
 import {User} from '../user/user';
 import {FormBuilder, NgModel} from '@angular/forms';
 import {UserService} from '../services/user.service';
-import { MyPipe } from './user-list.pipe';
+import {MyPipe} from './user-list.pipe';
 
 @Component({
   selector: 'app-user-list',
@@ -10,8 +10,8 @@ import { MyPipe } from './user-list.pipe';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  public sugestie: any[] = [];
   public uzytkownicy: any[] = [];
-  // public uzytkownik: any[] = [];
   uzytkownicy1 = [
     {id: 1, nick: 'user1', opis: 'user1', points: 69},
     {id: 2, nick: 'user2', opis: 'user2', points: 69},
@@ -23,28 +23,35 @@ export class UserListComponent implements OnInit {
     {id: 2, nick: 'user21', opis: 'user21', points: 69},
     {id: 1, nick: 'userer11', opis: 'user11', points: 69},
     {id: 1, nick: 'userek11', opis: 'user11', points: 69},
-    {id: 3, nick: 'user31', opis: 'user31', points: 69}
+    {id: 3, nick: 'user31', opis: 'user31', points: 69},
+    {id: 3, nick: 'edek', opis: 'user31', points: 69}
   ];
 
-  wzorzec: string = '11';
+  public wzorzec = '';
 
   public zalogowanyUzytkownik = JSON.parse(localStorage.getItem('data'));
+
   constructor(private formBuilder: FormBuilder,
-              private userService: UserService, 
+              private userService: UserService,
               private ref: ChangeDetectorRef) {
   }
 
   public ngOnInit() {
-    this.getAllUzytkownicy();
+    this.getAllSuggestions();
+    this.getAllUser();
     this.ref.detectChanges();
   }
 
-  public update() {
-    this.ref.detectChanges();
-  }
-
-  public getAllUzytkownicy() {
+  public getAllSuggestions() {
     this.userService.getSuggestions(this.zalogowanyUzytkownik.id).subscribe((result: any[]) => {
+      this.sugestie = result;
+      console.log(result);
+    }, (error) => {
+    });
+  }
+
+  public getAllUser() {
+    this.userService.getAllUser().subscribe((result: any[]) => {
       this.uzytkownicy = result;
       console.log(result);
     }, (error) => {
