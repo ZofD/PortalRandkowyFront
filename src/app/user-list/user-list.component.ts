99f95e79ebf0bd39eefd1737b5755d1c0daff409
banaskrzysztof <47889449+banaskrzysztof@users.noreply.github.com>
2020-06-20 20:16:19 +0200

    Wyszukiwanie z danych z bazy

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,8 +1,7 @@
 import {ChangeDetectorRef, Component, OnInit, Input} from '@angular/core';
-import {User} from '../user/user';
 import {FormBuilder, NgModel} from '@angular/forms';
 import {UserService} from '../services/user.service';
-import {MyPipe} from './user-list.pipe';
+
 
 @Component({
   selector: 'app-user-list',
@@ -12,21 +11,6 @@ import {MyPipe} from './user-list.pipe';
 export class UserListComponent implements OnInit {
   public sugestie: any[] = [];
   public uzytkownicy: any[] = [];
-  uzytkownicy1 = [
-    {id: 1, nick: 'user1', opis: 'user1', points: 69},
-    {id: 2, nick: 'user2', opis: 'user2', points: 69},
-    {id: 3, nick: 'user3', opis: 'user3', points: 69}
-  ];
-
-  uzytkownicy2 = [
-    {id: 1, nick: 'user11', opis: 'user11', points: 69},
-    {id: 2, nick: 'user21', opis: 'user21', points: 69},
-    {id: 1, nick: 'userer11', opis: 'user11', points: 69},
-    {id: 1, nick: 'userek11', opis: 'user11', points: 69},
-    {id: 3, nick: 'user31', opis: 'user31', points: 69},
-    {id: 3, nick: 'edek', opis: 'user31', points: 69}
-  ];
-
   public wzorzec = '';
 
   public zalogowanyUzytkownik = JSON.parse(localStorage.getItem('data'));
