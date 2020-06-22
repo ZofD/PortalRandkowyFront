2b694ebd7edb45b95689238955e8189af65a1e89
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-06-22 17:14:25 +0200

    czat

--- src/app/czat/czat.component.ts
+++ src/app/czat/czat.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./czat.component.css']
 })
 export class CzatComponent implements OnInit {
+  uzytkownicy: any[] = [];
+  wiadomosci: any[] = [
+    {mesage: 'moja wiadomosc', user: true},
+    {mesage: 'nie moja wiadomosc', user: false},
+    {mesage: 'moja wiadomosc', user: true}
+  ]
+  wzorzec: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public isMyMessage(user: boolean): boolean {
+    // if(user === curentUser)
+    return user;
   }
 
 }
